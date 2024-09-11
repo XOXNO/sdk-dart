9ad7084fd54648d1e28778c0cd9d0679b83f5a68
kevin <kevin@sakemaer.fr>
2024-09-11 11:41:41 +0200

    export raw api

--- lib/src/api.dart
+++ lib/src/api.dart
@@ -2,6 +2,17 @@ export 'package:xoxno_sdk/src/api/exception/exception.dart';
 
 export 'package:xoxno_sdk/src/api/client.dart';
 
+export 'package:xoxno_sdk/src/api/raw/ash_aggregator/ash_aggregator.dart';
+export 'package:xoxno_sdk/src/api/raw/chat/chat.dart';
+export 'package:xoxno_sdk/src/api/raw/collection/collection.dart';
+export 'package:xoxno_sdk/src/api/raw/liquid/liquid.dart';
+export 'package:xoxno_sdk/src/api/raw/nft/nft.dart';
+export 'package:xoxno_sdk/src/api/raw/nft_activity/nft_activity.dart';
+export 'package:xoxno_sdk/src/api/raw/search/search.dart';
+export 'package:xoxno_sdk/src/api/raw/staking/staking.dart';
+export 'package:xoxno_sdk/src/api/raw/tokens/tokens.dart';
+export 'package:xoxno_sdk/src/api/raw/transactions/transactions.dart';
+export 'package:xoxno_sdk/src/api/raw/user/user.dart';
 export 'package:xoxno_sdk/src/api/raw/api.dart';
 
 export 'package:xoxno_sdk/src/api/type_safe/tokens/category.dart';
